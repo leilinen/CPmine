743ba3e019816bca94d935e46fb08c76866976da
leilinen <lamlee1993@outlook.com>
2018-10-22 23:36:00 +0800

    todo list src

---
+++ front/vuejs/todolist/README.md
@@ -0,0 +1,30 @@
+# 慕课网vuejs入门课程实战项目 -- todolist
+
+[视频链接](https://www.imooc.com/learn/980)
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ front/vuejs/todolist/src/TodoList.vue
@@ -0,0 +1,67 @@
+<template>
+  <!--<div id="app">-->
+  <!--<img src="./assets/logo.png">-->
+  <!--<HelloWorld/>-->
+<!--</div>-->
+  <div>
+    <div>
+      <input v-model="inputValue"/>
+      <button @click="handleSubmit">提交</button>
+    </div>
+    <ul>
+      <todo-item
+        v-for="(item, index) of list"
+        :content="item"
+        :key="index"
+        :index="index"
+        @delete="handleDelete"
+      >
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import TodoItem from './components/TodoItem'
+// import HelloWorld from './components/HelloWorld'
+//
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+
+export default {
+  components: {
+    'todo-item': TodoItem
+  },
+  data: function () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
